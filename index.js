65ef6e79e84b04152fabcc7b22e3329b79eeb4d3
PabloFerrerDalmau <pablo.ferrerdalmau@enti.cat>
2021-04-30 11:19:23 +0200

    Classe 30/04/2021

--- index.js
+++ index.js
@@ -9,48 +9,62 @@ let server_url = "mongodb://localhost:27017";
 
 let chat_db
 
-mongo.connect(server_url, (err, server) => {
-  if (err){
+mongo.connect(server_url, (error, server) => {
+  if (error){
 	  console.log("Error en la conexión a MongoDB");
-	  throw err;
+	  throw error;
   }
 	            
   console.log("Dentro de MongoDB");
 	
   chat_db = server.db("amongmeme");
 
-  //chat_db.collection("chat").find({});
-
 });
 
-
 console.log("Inicializando servidor chat");
-
 
 let public_files = new node_static.Server("pub");
 
 http.createServer( (request, response) => {
-
-	console.log("Archivo"+request.url);
+	
 	if (request.url == "/chat"){
-		console.log("Entrando en el chat");
+		//console.log("Entrando en el chat");
 	let cursor = chat_db.collection("chat").find({});
-		
+
 		cursor.toArray().then( (data) => {
-		console.log(data);
+			//console.log(data);
+
+			response.writeHead(200, {'Content-Type':'text/plain'});
+
+			response.write (JSON,stringfy(data) );
+			response.end();
+		});
+
+		return;
+	}
+	if (request.url == "/submit"){
+		console.log("Envio de datos");
 		
+		let body = [];
+		request.on('data', chunk => {
+			body.push(chunk);
+		}).on('end', () => {
+			let chat_data = JSON.parse(Buffer.concat(body).toString());
 			
-		response.writeHead(200, {'Content-Type': 'text/plain'});		
-		
-		response.write ( JSON,stringify(data) );
+			chat_db.collection("chat").insertOne({
+				user:chat_data.user.chat_user,
+				msg:chat_data.chat_msg,
+				date: Date.now()
+			});
+		});
 
 		response.end();
-		});
-	
-		return;		
-		};
+		
+		return;
+	}
 	
 	public_files.serve(request, response);
+	
 
 }).listen(8080);
 
